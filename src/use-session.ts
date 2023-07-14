84f6aa44b81d0c23b0163108848d1fca3daa2105
Chris Boveda <cboveda@asu.edu>
2023-07-14 07:43:26 -0700

    Create useSession hook

---
+++ src/use-session.ts
@@ -0,0 +1,79 @@
+import { RealtimeChannel, Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { supaClient } from "./supa-client";
+
+export interface UserProfile {
+  username: string;
+  avatarUrl?: string;
+}
+
+export interface SupashipUserInfo {
+  session: Session | null;
+  profile: UserProfile | null;
+}
+
+export function useSession(): SupashipUserInfo {
+  const [userInfo, setUserInfo] = useState<SupashipUserInfo>({
+    profile: null,
+    session: null,
+  });
+  const [channel, setChannel] = useState<RealtimeChannel | null>(null);
+
+  useEffect(() => {
+    void supaClient.auth.getSession().then(({ data: { session } }) => {
+      setUserInfo((info) => {
+        return { ...info, session };
+      });
+      supaClient.auth.onAuthStateChange((_event, session) => {
+        setUserInfo({ session, profile: null });
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (userInfo.session?.user && !userInfo.profile) {
+      void listenToUserProfileChanges(userInfo.session.user.id).then(
+        (newChannel) => {
+          if (channel) {
+            void channel.unsubscribe();
+          }
+          setChannel(newChannel);
+        }
+      );
+    } else if (!userInfo.session?.user) {
+      void channel?.unsubscribe();
+      setChannel(null);
+    }
+  }, [userInfo.session]); //eslint-disable-line react-hooks/exhaustive-deps
+  /* Study Question:
+     How do I add channel and userInfo as dependencies (as the eslint rule suggests
+     I should) if I don't want them to trigger the callback?
+  */
+
+  async function listenToUserProfileChanges(userId: string) {
+    const { data } = await supaClient
+      .from("user_profiles")
+      .select("*")
+      .filter("user_id", "eq", userId);
+    if (data?.[0]) {
+      setUserInfo({ ...userInfo, profile: data?.[0] as UserProfile });
+    }
+    return supaClient
+      .channel("public:user_profiles")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "user_profiles",
+          filter: `user_id=eq.${userId}`,
+        },
+        (payload) => {
+          setUserInfo({ ...userInfo, profile: payload.new as UserProfile });
+        }
+      )
+      .subscribe();
+  }
+
+  return userInfo;
+}
